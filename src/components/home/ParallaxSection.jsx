1e656665f0614bc69a8890ad94b65901ef0ab867
Patrick Gray <patrickgray1996@gmail.com>
2024-11-08 12:11:49 +1300

    fixed parallax so only works on desktop screens

--- src/components/home/ParallaxSection.jsx
+++ src/components/home/ParallaxSection.jsx
@@ -3,7 +3,7 @@ import Footstool from '../../Images/Footstool.jpg'
 const ParallaxSection = () => {
   return (
     <div 
-      className="h-[70vh] bg-fixed bg-center bg-no-repeat bg-cover flex items-center justify-center relative"
+      className="h-[70vh] bg-scroll md:bg-fixed bg-center bg-no-repeat bg-cover flex items-center justify-center relative"
       style={{ backgroundImage: `url(${Footstool})` }}
     >
       <div className="absolute inset-0 bg-black/30"></div>
